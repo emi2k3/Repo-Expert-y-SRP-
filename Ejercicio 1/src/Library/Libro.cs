280fbd975f0f57b10f54d94d8a0bf2c2cd7c66a5
Emilio Rodríguez <emilio.rodriguez@correo.ucu.edu.uy>
2022-09-11 22:17:31 -0300

    Ejercicio 1 completado

--- Ejercicio 1/src/Library/Libro.cs
+++ Ejercicio 1/src/Library/Libro.cs
@@ -4,12 +4,10 @@ namespace SRP
 {
     public class Book
     {
-
         public string Title { get ; }
         public string Author { get ; }
         public string Code { get ;  }
-        public string LibrarySector { get ; set; }
-        public string LibraryShelve { get ; set; }
+       //Para crear un libro se necesita conocer el título el autor y el código del mismo. La clase solo se encarga de crear el mismo
 
         public Book(String title, String author, String code)
         {
@@ -18,11 +16,30 @@ namespace SRP
             this.Code = code;
         }
 
-        public void ShelveBook(String sector, String shelve)
+   
+
+    }
+
+    public class Shelve{
+         public string LibrarySector { get; }
+         public string LibraryShelve { get; }
+         // Se conoce el sector y el número donde se guardara el libro y luego se crea el estante.
+
+             public Shelve(String sector, String shelve)
         {
             this.LibrarySector = sector;
             this.LibraryShelve = shelve;
         }
+    }
+
+    public class BookinShelve{
+        public Book SavedBook{ get; }
+        public Shelve SavedShelve{ get;}
+        // Al conocer el libro y el estante donde se va a guardar, se crea el objeto con el libro y la estanteria dentro.
+            public BookinShelve(Book SavedBook, Shelve SavedShelve){
+                this.SavedBook=SavedBook;
+                this.SavedShelve=SavedShelve;
+            }
 
     }
 }
--- Ejercicio 1/src/Program/Program.cs
+++ Ejercicio 1/src/Program/Program.cs
@@ -9,8 +9,13 @@ namespace SRP
         {
             Book book1 = new Book("Design Patterns","Erich Gamma & Others","001-034");
             Book book2 = new Book("Pro C#","Troelsen","001-035");
-            book1.ShelveBook("A","7");
-            book2.ShelveBook("B","3");
+            // Se crean libros.
+            Shelve shelve1 = new Shelve("A","7");
+            Shelve shelve2 = new Shelve("B","3");
+            // Se crean estanterias.
+            BookinShelve newbookinshelve = new BookinShelve(book1,shelve1);
+            BookinShelve newbookinshelve2 = new BookinShelve(book2,shelve2);
+            // Se crea las estanterias con los libros dentro.
 
         }
     }
